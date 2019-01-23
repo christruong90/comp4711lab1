9645d09f02c6fa508a1ac8114212d1446004f7f4
Chris Truong <chrisanhoi@hotmail.com>
2019-01-22 16:10:26 -0800

    put mc questions in a subarray within json

--- labs/2/js/dynamicQuiz.js
+++ labs/2/js/dynamicQuiz.js
@@ -1,25 +1,18 @@
 var quiz = [
 		{
-			question:"<p> 1) Which code snippet can be used to create a new Date Object?</p>" +
-					"<form>" +
-						"<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new </span>Date() <br>" +
-						"<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new</span> date_create(\'2019-01-10\') <br>" +
-						"<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new</span> current_date() <br>" +
-						"<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new</span> get_date() <br>" +
-					"</form>",
+			question:"<p> 1) Which code snippet can be used to create a new Date Object?</p>",
+			choices: ["<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new </span>Date()<br>",
+			 "<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new</span> date_create(\'2019-01-10\') <br>", 
+			 "<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new</span> current_date() <br>", 
+			 "<input type=\"radio\" name=\"q1\"> <span style =\"color:purple\">new</span> get_date() <br>"],
 			answer: 1
 		},
 		{
-			question:"<p> 2) What is the difference between \"==\" and \"===\"?</p>" +
-			"<form>" +
-				"<input type=\"radio\" name=\"q2\"> <span style =\"color:red\">\"==\"</span> is comparison and <span style =\"color:red\">\"===\"" +
-				"</span> is assignment <br>" +
-				"<input type=\"radio\" name=\"q2\"> <span style =\"color:red\">\"==\"</span> is assignment and <span style =\"color:red\">\"" + "===\"" +
-				"</span> is comparison <br>" +
-				"<input type=\"radio\" name=\"q2\"> <span style =\"color:red\">\"==\"</span> will check comparison after conversion of data types" +  
-				"and <span style =\"color:red\">\"===\"</span> will compare without conversion of data type <br>" +
-				"<input type=\"radio\" name=\"q2\"> They are the both identical <br>" +	
-			"</form>",
+			question:"<p> 2) What is the difference between \"==\" and \"===\"?</p>",
+			choices: ["<input type=\"radio\" name=\"q2\"> <span style =\"color:red\">\"==\"</span> is comparison and <span style =\"color:red\">\"===\"" +
+				"</span> is assignment <br>", "<input type=\"radio\" name=\"q2\"> <span style =\"color:red\">\"==\"</span> is assignment and <span style =\"color:red\">\"" + "===\"" +
+				"</span> is comparison <br>", "<input type=\"radio\" name=\"q2\"> <span style =\"color:red\">\"==\"</span> will check comparison after conversion of data types" +  
+				"and <span style =\"color:red\">\"===\"</span> will compare without conversion of data type <br>", "<input type=\"radio\" name=\"q2\"> They are the both identical <br>"],
 			answer: 2		
 		},
 		{
@@ -28,28 +21,20 @@ var quiz = [
 				"pens = [<span style =\"color:red\">\"red\"</span>, <span style =\"color:red\">\"blue\"</span>, <span style =\"color:red\">\"green\"</span>," +
 				"<span style =\"color:red\">\"orange\"</span>];<br><br>" +
 				"<span style =\"color:green\">var </span> color = pens[2];<br>" +
-				"<span style =\"color:blue\">console</span>.log(color);<br>" +
-
-			"<form>" +
-				"<input type=\"radio\" name=\"q3\"> \"red\" <br>" +
-				"<input type=\"radio\" name=\"q3\"> \"blue\" <br>" + 
-				"<input type=\"radio\" name=\"q3\"> \"green\" <br>" +
-				"<input type=\"radio\" name=\"q3\"> \"orange\" <br>" +	
-			"</form>",
+				"<span style =\"color:blue\">console</span>.log(color);<br><br>",
+			choices:["<input type=\"radio\" name=\"q3\"> \"red\" <br>",
+					"<input type=\"radio\" name=\"q3\"> \"blue\" <br>",
+					"<input type=\"radio\" name=\"q3\"> \"green\" <br>",
+					"<input type=\"radio\" name=\"q3\"> \"orange\" <br>"],
 			answer: 3
 		},
 		{
 			question:"<p> 4) What will be the output of the following code snippet? if a = 1 and b = 2<p>" +
 				"a == b ? <span style =\"color:blue\">console</span>.log(<span style =\"color:red\">\"Match\"</span>) : <span style =\"color:blue\">console</span>.log(<span style =\"" +
-				"color:red\">\"No Match\"</span>);" +
+				"color:red\">\"No Match\"</span>);",
 			
-
-			"<form>" +
-				"<input type=\"radio\" name=\"q4\"> Match <br>" +
-				"<input type=\"radio\" name=\"q4\"> No Match <br>" +
-				"<input type=\"radio\" name=\"q4\"> Undefined <br>" +
-				"<input type=\"radio\" name=\"q4\"> NaN <br>" +	
-			"</form>",
+			choices:["<input type=\"radio\" name=\"q4\"> Match <br>", "<input type=\"radio\" name=\"q4\"> No Match <br>",
+					"<input type=\"radio\" name=\"q4\"> Undefined <br>", "<input type=\"radio\" name=\"q4\"> NaN <br>" ],
 			answer: 4 
 		}, 
 		{ 
@@ -62,17 +47,11 @@ var quiz = [
 			"}<br>" +
 			"<span style =\"color:green\">var</span> y;<br><br>" +
 
-			"What type of scope is <span style =\"color:green\">var</span> x and <span style =\"color:green\">var</span> y??<br><br>" +
-
-
-			"<form>" +
-				"<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> x = global scope and <span style =\"color:green\">var</span> y = local scope <br>" +
-				"<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> y = global scope and <span style =\"color:green\">var</span> x = local scope <br>" +
-				"<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> x = block scope and <span style =\"color:green\">var</span> y = local scope <br>" +
-				"<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> x = global scope and <span style =\"color:green\">var</span> y = block scope <br>" +
-			"</form>" +
-			"<br>" +
-			"<br>",
+			"What type of scope is <span style =\"color:green\">var</span> x and <span style =\"color:green\">var</span> y??<br><br>",
+			choices: ["<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> x = global scope and <span style =\"color:green\">var</span> y = local scope <br>",
+					"<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> y = global scope and <span style =\"color:green\">var</span> x = local scope <br>", 
+					"<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> x = block scope and <span style =\"color:green\">var</span> y = local scope <br>",
+					"<input type=\"radio\" name=\"q5\"> <span style =\"color:green\">var</span> x = global scope and <span style =\"color:green\">var</span> y = block scope <br>"],
 			answer: 5
 
 		} 
@@ -86,7 +65,12 @@ var quiz = [
 		if(userInput >0 && userInput < 6) {
 			for (let i = 0; i < userInput; i++) {
 				document.writeln(quiz[i].question);
-				document.writeln("<br>")
+				// document.writeln("<br>")
+				document.writeln("<form>")
+				for (let j = 0; j < 4; j++) {
+					document.writeln(quiz[i].choices[j])
+				}
+				document.writeln("</form>")
 			}
 		} else {
 			alert("must be a number between 0 - 5")
