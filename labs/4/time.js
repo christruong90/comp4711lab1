3b27dca2689b57e3b92a8279b8f6dac0b336b115
Chris Truong <chrisanhoi@hotmail.com>
2019-02-05 18:00:49 -0800

    fixed time.js

--- labs/4/time.js
+++ labs/4/time.js
@@ -2,17 +2,22 @@ var http = require('http');
 var fs = require('fs');
 // var numberOfRequests = 0;
 
+let today = new Date();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+// document.getElementById("test1").innerHTML = time;
+
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-type': 'text/html'});
+    response.writeHead(200, {'Content-type': 'text/plain'});
     fs.readFile('index.html', null, function(error, data) {
         if(error) {
             response.writeHead(404);
             response.write('File not found');
         } else {
-            response.write(data);
+            response.write(time);
         }
         response.end()
     });
 }
 console.log('listening ...');
-http.createServer(onRequest)
+http.createServer(onRequest).listen(8000)
