4004d30d8155c3dc0258d606c552f53867619d73
Chris Truong <chrisanhoi@hotmail.com>
2019-02-07 17:49:17 -0800

    fix2

--- labs/test/nodeServer/index.js
+++ labs/test/nodeServer/index.js
@@ -6,7 +6,7 @@ let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end(today);
+    response.end("HELLO WORLD");
 
 });
 
